fa523ca01ba2d8279ab43ed1125b92d7b1f4ba68
QIN-YE-DOU-XIN <3014815021@qq.com>
2025-01-10 13:58:15 +0800

    3 save

--- 01.py
+++ 01.py
@@ -4,7 +4,5 @@ with open("001.txt", "r+", encoding="utf-8") as file:
     # 选择奇数行（索引从0开始，所以0, 2, 4...是奇数行）
     odd_lines = [lines[i] for i in range(len(lines)) if i % 2 == 0]
     print(odd_lines)
-    file.seek(0)  # 将文件指针移到文件开头，准备覆盖原有内容
-    file.truncate(0)  # 清空文件内容
-    file.writelines(odd_lines)
+
     """1123"""
